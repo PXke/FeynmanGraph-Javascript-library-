afe40b5a2fe6c72e22ebe8cfa4f3af671e9ee876
Lastecoueres Guillaume <PX9e@gmx.fr>
2012-10-29 23:24:10 +0100

    MVC applied

--- js/FGJL.js
+++ js/FGJL.js
@@ -5,8 +5,16 @@ function FGJL(ZoneToDraw) {
 	var widthChoice = document.getElementById("ChoiceZone").offsetWidth;
 	var Particle = new Array("e-", "e+", "t", "b", "s", "c", "u", "d", "\u02e0");
 	var quadrillageOn=1;
-	var	MaxChoicePrint= heightArea/widthChoice + 1 ;
+	var Lines = new Array();
+	var	CaseToDraw= heightArea/widthChoice + 1 ;
+	var IDStartCase=0;
+	var IDEndCase=IDStartCase+CaseToDraw;
 	var Case= new Array();
+	var Chart;
+	var ChoiceZone;
+	var UpdateDrawZone=1;
+	var UpdateChoiceZone=1;
+	
 
 	//This Object represents a particle. It contains Typical information about this on and its graphic representation
 	function particle(ID,LineIdP, OrderP,graphicRepresentationP) {
@@ -21,9 +29,9 @@ function FGJL(ZoneToDraw) {
 	
 	
 	//This Object represents a line of a the GUI.The idea is to be able to know on which line we wish to print our particle(Vertice).
-	function line(numberP, graphicRepresentationP) {
+	function line(numberP) {
 		this.Number = numberP;
-		this.graphicRepresentation = graphicRepresentationP;
+		
 		return this;
 	}
 	
@@ -33,10 +41,53 @@ function FGJL(ZoneToDraw) {
 		return this;
 	}
 	
+	//This function is here to draw what have to be drawn  !
+	GUI = function(){
+		
+		var i = 0;
+		
+		if(UpdateDrawZone==1)
+		{
+		//If the quadrillage option is on we draw line ! else we don't !
+			if(quadrillageOn==1)
+			{
+				var StepX=widthArea/(1+line.length);
+				
+				for(i=0;i<line.length;i++)
+				{
+					Chart.append("svg:line").attr("x1",StepX*(i+1)).attr("x2",StepX*(i+1)).attr("y1",0).attr("y2",heightArea).style("stroke","#CCCCCC");
+				}
+			}
+			UpdateDrawZone=0;
+		}
+		if(UpdateChoiceZone==1)
+		{
+			
+			for(i=0;i<CaseToDraw;i++)
+			{
+			
+				ChoiceZone.append("svg:line").attr("x1",0).attr("x2",0).attr("y1",i*widthChoice).attr("y2",(i+1)*widthChoice-20).style("stroke","black");
+				ChoiceZone.append("svg:line").attr("x1",StepX*(i+1)).attr("x2",StepX*(i+1)).attr("y1",0).attr("y2",0).style("stroke","#CCCCCC");
+				ChoiceZone.append("svg:line").attr("x1",StepX*(i+1)).attr("x2",StepX*(i+1)).attr("y1",0).attr("y2",heightArea).style("stroke","#CCCCCC");
+				ChoiceZone.append("svg:line").attr("x1",StepX*(i+1)).attr("x2",StepX*(i+1)).attr("y1",0).attr("y2",heightArea).style("stroke","#CCCCCC");
+				
+			}
+		}
+		
+		
+	}
 	
 	
-	var Chart = d3.select("#" + ZoneToDraw).append("svg").attr("class", "chart").attr("width", widthArea).attr("height", heightArea);
-	var ChoiceZone = d3.select("#ChoiceZone").append("svg").attr("class", "choicezone").attr("width", widthChoice).attr("height", heightArea);
+	Init = function(){
+		Chart = d3.select("#" + ZoneToDraw).append("svg").attr("class", "chart").attr("width", widthArea).attr("height", heightArea);
+		ChoiceZone = d3.select("#ChoiceZone").append("svg").attr("class", "choicezone").attr("width", widthChoice).attr("height", heightArea);
+		Lines.push(new line(1));
+		
+	}
+	
+	Init();
+	GUI();
+	
 	
 }
 
